9617087e6ab5dccafbd27a2bc07b2efa08ecfa0b
Prins Kamariya <88283873+prins2003@users.noreply.github.com>
2022-07-01 17:09:38 +0530

    Add files via upload

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+
+export const getPlacesData = async (type,sw,ne) => {
+    try{
+        // request
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
+            params: {
+            bl_latitude: sw.lat,
+            tr_latitude:ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Host': ''
+          }
+        });
+
+        return data;
+    } catch(error){
+        console.log(error);
+    }
+}
